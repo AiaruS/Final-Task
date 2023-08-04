8b59d017fd56c125e55f6f2438b813ae3ebea4eb
Aiaru <shakhanova.a2@gmail.com>
2023-08-05 04:13:05 +0600

    add PrintArray

--- FinalTask_Array/Program.cs
+++ FinalTask_Array/Program.cs
@@ -5,10 +5,19 @@ void Method4(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++);
     {
-    if (array1[i].Length <= 3)
-    {
+        if (array1[i].Length <= 3)
+        {
+            return;
+        }
         array2[count] = array1[i];
         count++;
     }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
     }
+    Console.WriteLine();
 }
